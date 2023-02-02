e681819baf8c4105b4d6fd55eae5e3892cfb04b8
mariia.kitaieva <kitaievam@ukr.net>
2023-02-02 15:49:21 +0200

    script for getting OS name was written

---
+++ OS_name_script.py
@@ -0,0 +1,7 @@
+import platform, os
+OS_info = platform.uname()
+#OS_info2 = platform.system()
+#OS_info3 = platform.release()
+#OS_info4 = platform.os.name
+print(OS_info)
+print("the name of OS is: ", OS_info[0], OS_info[2])
