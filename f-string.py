6dd83bd046470f4f810795d005c6fb8f9c64191b
mariia.kitaieva <kitaievam@ukr.net>
2023-02-22 15:26:22 +0200

    task 3 was written

--- f-string.py
+++ f-string.py
@@ -2,7 +2,6 @@
 2 There is a list of friends [""John"", ""Marta"", ""James"", ""Amanda"", ""Marianna""].
  print to the console the names of each on a new line, right-aligned using the string method and formatting via f string.
  Also, above the names, in the first line, display the headings Names where the word names should be in the middle, and the rest of the space is filled with the symbol ""*""
-
 3 There is a string "" name=Amanda=sssss&age=32&&salary=1500&currency=euro "".
  Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: quro}'''
 
@@ -15,15 +14,14 @@ print(f'{names_string.title()}')
 #2
 
 #3 Dictionary
-mess_string = 'name=Amanda=sssss&age=32&&salary=1500&currency=euro'
-print(mess_string.split('='))
-for el in mess_string:
-    if el.isalpha():
-        print(el, end='')
-    else:
-        continue
-print()
-
+# mess_string = 'name=Amanda=sssss&age=32&&salary=1500&currency=euro'
+# print(mess_string.split('='))
+# for el in mess_string:
+#     if el.isalpha():
+#         print(el, end='')
+#     else:
+#         continue
+# print()
 
 #4
 list1 = ['FirstItem', 'FriendsList', 'MyTuple']
