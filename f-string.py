8b7173f7622850edb7767ba8e50c50f8e5fe1032
mariia.kitaieva <kitaievam@ukr.net>
2023-02-22 15:25:11 +0200

    script for getting OS name was written

--- f-string.py
+++ f-string.py
@@ -4,10 +4,7 @@
  Also, above the names, in the first line, display the headings Names where the word names should be in the middle, and the rest of the space is filled with the symbol ""*""
 
 3 There is a string "" name=Amanda=sssss&age=32&&salary=1500&currency=euro "".
- Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: quro}
-
-4 You have a list of variable names in camel case format [""FirstItem"", ""FriendsList"", ""MyTuple""]
- convert it to a list of variable names for python in snake case format [""first_item"", ""friends_list"", ""my_tuple""]"'''
+ Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: quro}'''
 
 
 #1 Capital letter
@@ -16,13 +13,6 @@ names_string = "john peter brian Morgan Adam Maria bart"
 print(f'{names_string.title()}')
 
 #2
-'''
-names_list = ["John", "Marta", "James", "Amanda", "Marianna"]
-#print("Tom has {0:<4} red {1:^10}!".format(5, "apples"))
-unpacked_list = *names_list, sep='\n'
-
-print(f"{f'${unpacked_list:0.2f}':*>20s}")
-
 
 #3 Dictionary
 mess_string = 'name=Amanda=sssss&age=32&&salary=1500&currency=euro'
@@ -33,10 +23,9 @@ for el in mess_string:
     else:
         continue
 print()
-'''
-# You have a list of variable names in camel case format [""FirstItem"", ""FriendsList"", ""MyTuple""]
-#  convert it to a list of variable names for python in snake case format [""first_item"", ""friends_list"", ""my_tuple""
 
+
+#4
 list1 = ['FirstItem', 'FriendsList', 'MyTuple']
 list2 = []
 for el in list1:
